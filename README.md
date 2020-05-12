6cac5b6adceab7658992bacb47bc6dfe20f9deec
mohith227 <mohithnanjundamurthy05@gmail.com>
2020-05-12 10:03:30 +0530

    php socket programming

---
+++ README.md
@@ -0,0 +1,17 @@
+# php_socket_programming
+
+#system requirement
+
+php 7.1 and above
+
+add cboden/ratchet in composer
+
+install ngrok
+
+#things to execute
+
+composer update
+
+php app.php
+
+ngrok http 80
---
+++ app/Socket.php
@@ -0,0 +1,42 @@
+<?php
+namespace MyApp;
+
+use Ratchet\MessageComponentInterface;
+use Ratchet\ConnectionInterface;
+
+class Socket implements MessageComponentInterface
+{
+    public function __construct()
+    {
+        $this->clients = new \SplObjectStorage;
+    }
+
+    public function onOpen(ConnectionInterface $conn)
+    {
+// Store the new connection in $this->clients
+        $this->clients->attach($conn);
+        echo "New connection! ({$conn->resourceId})\n";
+    }
+
+    public function onMessage(ConnectionInterface $from, $msg)
+    {
+        foreach ($this->clients as $client) {
+            if ($from->resourceId == $client->resourceId) {
+                continue;
+            }
+            $client->send("Client $from->resourceId said $msg");
+        }
+    }
+
+    public function onClose(ConnectionInterface $conn)
+    {
+        $this->clients->detach($conn);
+        echo "Connection {$conn->resourceId} has disconnected\n";
+    }
+
+    public function onError(ConnectionInterface $conn, \Exception $e)
+    {
+        echo "An error has occurred: {$e->getMessage()}\n";
+        $conn->close();
+    }
+}
